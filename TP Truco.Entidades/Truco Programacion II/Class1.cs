d0962781be96fd5d03cd9aa2d1e564ca38e2666e
MauriRosso <maurirosso@hotmail.com>
2017-04-12 19:16:04 -0300

    TP Truco
    Truco sanmartino, rodriguez, rosso

---
+++ TP Truco.Entidades/Truco Programacion II/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truco_Programacion_II
+{
+    public class Class1
+    {
+    }
+}
